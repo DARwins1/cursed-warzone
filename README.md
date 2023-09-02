60c7b4cae813d3ccbdcbc0338421cb975028cfdf
IHateOrangeRocks <littlemacweir@gmail.com>
2023-09-01 21:44:16 -0700

    Added Beta 2.
    It includes Beta 2, as in all the relevant map files and scripting for it. There is also a slight change to the README.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cursed-warzone
+This mod is a fresh experience that reimagines the standard Warzone campaign by introducing various balance changes and visual tweaks.
---
+++ script/campaign/cam2-1s.js
@@ -0,0 +1,15 @@
+// convert this to JS file later
+// this is the setup level for Beta 2
+
+include("script/campaign/libcampaign.js");
+
+function eventStartLevel()
+{
+	camSetupTransporter(87, 100, 70, 1);
+	centreView(88, 101);
+	setNoGoArea(86, 99, 88, 101, CAM_HUMAN_PLAYER);
+	setMissionTime(camChangeOnDiff(camMinutesToSeconds(70)));
+	camSetStandardWinLossConditions(CAM_VICTORY_PRE_OFFWORLD, "SUB_2_1");
+
+        enableResearch("R-Wpn-MG3Mk1", CAM_HUMAN_PLAYER); // Realistic Heavy Machinegun (from Spamton)
+}
